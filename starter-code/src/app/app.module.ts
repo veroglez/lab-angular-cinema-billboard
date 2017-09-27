55d891f64aa04e5ed0620d8913c05e88a1db2f96
veroglez <veronica.glezperez@gmail.com>
2017-09-27 18:51:20 +0200

    Iteration 4 done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { MyHomeComponentComponent } from './my-home-component/my-home-component.component';
 import { MyMovieComponentComponent } from './my-movie-component/my-movie-component.component';
 import { RouterModule, Routes } from "@angular/router";
+import { ActivatedRoute } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
--- starter-code/src/app/my-movie-component/my-movie-component.component.ts
+++ starter-code/src/app/my-movie-component/my-movie-component.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from '../services/Cinema.service';
 
 @Component({
   selector: 'app-my-movie-component',
   templateUrl: './my-movie-component.component.html',
-  styleUrls: ['./my-movie-component.component.css']
+  styleUrls: ['./my-movie-component.component.css'],
+  providers: [MoviesService]
 })
-export class MyMovieComponentComponent implements OnInit {
 
-  constructor() { }
+export class MyMovieComponentComponent implements OnInit {
+  moviesId:Number
+  constructor(private route: ActivatedRoute, private theCinema: MoviesService) { }
+  movies = this.theCinema.movies
 
   ngOnInit() {
+    this.route.params
+      .subscribe((params) => { console.log(params);this.moviesId = Number(params['id'])});
   }
 
 }
